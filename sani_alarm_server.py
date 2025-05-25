d400ae0c25d3ca8faefaa6e939ef7650fdce52e0
Faultiernotruf112 <philippmichel11@gmx.de>
2025-05-25 20:10:35 +0200

    Add files via upload

--- sani_alarm_server.py
+++ sani_alarm_server.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify
 import uuid
 import time
+import os
 
 app = Flask(__name__)
 
@@ -52,4 +53,5 @@ def list_devices():
     ])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
